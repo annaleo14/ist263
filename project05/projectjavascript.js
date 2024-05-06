86966df6ba38147fe44e8de313a602bd331a3c2c
annaleo14 <145688455+annaleo14@users.noreply.github.com>
2024-05-05 22:11:48 -0400

    Create projectjavascript.js

---
+++ project05/projectjavascript.js
@@ -0,0 +1,27 @@
+function waterHide () {
+    document.getElementById("water").style.display = "none";
+}
+
+function skyHide () {
+    document.getElementById("sky").style.display = "none";
+}
+
+function flowerHide () {
+    document.getElementById("flower").style.display = "none";
+}
+
+function campusHide () {
+    document.getElementById("campus").style.display = "none";
+}
+
+function miscellanousHide () {
+    document.getElementById("miscellanous").style.display = "none";
+}
+
+function showNav () {
+    document.getElementById("navlinks").style.dsplay = "block";
+}
+
+function hideNav () {
+    document.getElementById("navlinks").style.display = "none";
+}
